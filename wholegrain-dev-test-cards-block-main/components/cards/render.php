c4ef330abffee218bdb2c7567e3fd111477befe3
_s <43612522+prabin-tamang@users.noreply.github.com>
2025-01-20 19:43:27 +0000

    Update render.php

--- wholegrain-dev-test-cards-block-main/components/cards/render.php
+++ wholegrain-dev-test-cards-block-main/components/cards/render.php
@@ -4,7 +4,9 @@
     <?php while( have_rows('cards') ): the_row(); ?>
             <div class="col-sm-12 col-md-4 col-lg-4 cards__box py-3">
                 <div class="cards__header">
-                    <div class="cards__label"><?php the_sub_field('card_label_link'); ?></div>
+                    <?php if (get_sub_field('card_label_link')): ?>
+                        <div class="cards__label"><?php the_sub_field('card_label_link'); ?></div>
+                   <?php endif; ?>
                     <div class="cards__image">
                      <?php 
                         $image = get_sub_field('card_image');
@@ -17,12 +19,16 @@
                     </div>
                 </div>
                 <div class="cards__footer">
+                     <?php if (get_sub_field('card_title')): ?>
                     <div class="cards__title">
                         <h3><?php the_sub_field('card_title'); ?></h3>
                     </div>
+                <?php endif; ?>
+                    <?php if (get_sub_field('card_description')): ?>
                     <div class="cards__description">
                         <p><?php the_sub_field('card_description'); ?></p>
                     </div>
+                    <?php endif; ?>
                 </div>
           </div>
     <?php endwhile; ?>
