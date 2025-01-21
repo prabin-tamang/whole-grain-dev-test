269152a36a7f75037a49930dd2c6ac3d4d155bce
_s <43612522+prabin-tamang@users.noreply.github.com>
2025-01-21 10:36:13 +0000

    Update render.php

--- wholegrain-dev-test-cards-block-main/components/cards/render.php
+++ wholegrain-dev-test-cards-block-main/components/cards/render.php
@@ -1,38 +1,41 @@
-<?php if( have_rows('cards') ): ?>
-       <section class="cards">
-            <div class="row">
-    <?php while( have_rows('cards') ): the_row(); ?>
-            <div class="col-sm-12 col-md-4 col-lg-4 cards__box py-3">
-                <div class="cards__header">
-                    <?php if (get_sub_field('card_label_link')): ?>
-                        <div class="cards__label"><?php the_sub_field('card_label_link'); ?></div>
-                   <?php endif; ?>
-                    <div class="cards__image">
-                     <?php 
-                        $image = get_sub_field('card_image');
-                        $size = 'medium'; 
-                        if( $image ) {
-                            echo wp_get_attachment_image( $image, $size );
-
-                        }
-                        ?>
+<?php if (have_rows('cards')): ?>
+    <section class="cards">
+        <div class="row">
+            <?php while (have_rows('cards')): the_row(); ?>
+                <div class="col-sm-12 col-md-4 col-lg-4 cards__box py-3">
+                    <div class="cards__header">
+                        <?php if (get_sub_field('card_label_link')): ?>
+                            <div class="cards__label">
+                                <?php echo esc_html(get_sub_field('card_label_link')); ?>
+                            </div>
+                        <?php endif; ?>
+                        <div class="cards__image">
+                            <?php 
+                                $image = get_sub_field('card_image');
+                                $size = 'medium';
+                                if ($image) {
+                                    // Escape the image attributes
+                                    echo wp_get_attachment_image($image, $size, false, [
+                                        'alt' => esc_attr(get_post_meta($image, '_wp_attachment_image_alt', true))
+                                    ]);
+                                }
+                            ?>
+                        </div>
+                    </div>
+                    <div class="cards__footer">
+                        <?php if (get_sub_field('card_title')): ?>
+                            <div class="cards__title">
+                                <h3><?php echo esc_html(get_sub_field('card_title')); ?></h3>
+                            </div>
+                        <?php endif; ?>
+                        <?php if (get_sub_field('card_description')): ?>
+                            <div class="cards__description">
+                                <p><?php echo esc_html(get_sub_field('card_description')); ?></p>
+                            </div>
+                        <?php endif; ?>
                     </div>
                 </div>
-                <div class="cards__footer">
-                     <?php if (get_sub_field('card_title')): ?>
-                    <div class="cards__title">
-                        <h3><?php the_sub_field('card_title'); ?></h3>
-                    </div>
-                <?php endif; ?>
-                    <?php if (get_sub_field('card_description')): ?>
-                    <div class="cards__description">
-                        <p><?php the_sub_field('card_description'); ?></p>
-                    </div>
-                    <?php endif; ?>
-                </div>
-          </div>
-    <?php endwhile; ?>
-            </div>
-        </section>
+            <?php endwhile; ?>
+        </div>
+    </section>
 <?php endif; ?>
-
